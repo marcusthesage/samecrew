5c14d551510b41d5111f322e9663e039bc963941
marcusthesage <114116426+marcusthesage@users.noreply.github.com>
2024-09-04 22:06:40 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './index.js',
+    './faq.html',
+    './about.html',
+  ],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+      
+    },
+  },
+  plugins: [],
+}
+
